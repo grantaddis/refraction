08b07dc90b3c1110b12de4b8133d5fc5488ec9de
grantaddis <grant@addis.net>
2016-02-25 01:21:24 -0500

    safety commit

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject followSegment;
+    public GameObject followSegment;
 
-	private Vector3 offset;
+    private Vector3 offset;
 
-	void Start ()
-	{
-		offset = transform.position - followSegment.transform.position;
-	}
+    void Start ()
+    {
+        offset = transform.position - followSegment.transform.position;
+    }
 
-	void LateUpdate ()
-	{
-            Vector3 oscillatingLocation = followSegment.transform.position + offset;
-            transform.position = new Vector3(oscillatingLocation.x, 10.0f, oscillatingLocation.z);
-	}
+    void LateUpdate ()
+    {
+        Vector3 oscillatingLocation = followSegment.transform.position + offset;
+        transform.position = new Vector3(oscillatingLocation.x, 10.0f, oscillatingLocation.z);
+    }
 }
--- Assets/Scripts/WaveSegmentController.cs
+++ Assets/Scripts/WaveSegmentController.cs
@@ -4,7 +4,9 @@ using System;
 
 public class WaveSegmentController : MonoBehaviour {
 
-	public float wavelength, velocity;
+	public float wavelength, velocity, refractiveIndex;
+
+    private float prevMaterialPhase, prevRefractiveIndex, prevPhase, holdTime;
 
     private Rigidbody rb;
 	private Renderer rend;
@@ -13,15 +15,37 @@ public class WaveSegmentController : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
 		rend = GetComponent<Renderer> ();
 		rend.material.SetFloat ("_Velocity", velocity);
+		rend.material.SetFloat ("_TransitionPhase", 0.0f);
+		prevMaterialPhase = 0.0f;
+		prevPhase = 0.0f;
+		holdTime = 0.0f;
     }
     
     void FixedUpdate () {
+		if (transform.position.x > 10 && transform.position.x < 200) {
+			if (refractiveIndex != 0.5f) {
+				refractiveIndex = 0.5f;
+				rend.material.SetFloat ("_TransitionPhase", 10.0f);
+				holdTime = (2.0f / (refractiveIndex * velocity));
+				prevMaterialPhase = prevPhase;
+			}
+		} else {
+			if (refractiveIndex != 1.0f) {
+				refractiveIndex = 1.0f;
+				rend.material.SetFloat ("_TransitionPhase", 0.0f);
+				holdTime = (2.0f / (refractiveIndex * velocity));
+				prevMaterialPhase = prevPhase;
+			}
+		}
+
+		rend.material.SetFloat ("_RefractiveIndex", refractiveIndex);
+
         if (Input.GetAxis ("Vertical") > 0) {
             velocity += 1.0f;
 			rend.material.SetFloat ("_Velocity", velocity);
         } else if (Input.GetAxis ("Vertical") < 0) {
+			velocity -= 1.0f;
 			rend.material.SetFloat ("_Velocity", velocity);
-            velocity -= 1.0f;
         }
 
 		if (Input.GetAxis ("Horizontal") > 0) {
@@ -30,13 +54,21 @@ public class WaveSegmentController : MonoBehaviour {
             }
 			rend.material.SetFloat ("_Wavelength", wavelength);
 		} else if (Input.GetAxis ("Horizontal") < 0) {
-            if (wavelength > 0.485) {
+            if (wavelength > 0.486) {
 			    wavelength -= 0.0001f;
             }
 			rend.material.SetFloat ("_Wavelength", wavelength);
 		}
         
-		Vector3 newPos = new Vector3 (transform.position.x + velocity * Time.deltaTime, transform.position.y, transform.position.z);
-		rb.MovePosition(newPos);
+		if (holdTime == 0.0f) {
+			Vector3 newPos = new Vector3 (transform.position.x + velocity * refractiveIndex * Time.deltaTime, transform.position.y, transform.position.z);
+			rb.MovePosition (newPos);
+		} else if (Time.deltaTime > holdTime) {
+			holdTime = 0.0f;
+		} else {
+			holdTime -= Time.deltaTime;
+		}
+
+		prevPhase = prevMaterialPhase + ((2.0f * Mathf.PI / wavelength) * (Time.time * velocity * refractiveIndex + transform.position.x));
     }
 }
