8f5528b15cc8dfbd97d1bd35e7478fc028b75def
grantaddis <grant@addis.net>
2016-02-26 03:16:19 -0500

    full smaple implementation of velocity modulation

--- Assets/Scripts/WaveSegmentController.cs
+++ Assets/Scripts/WaveSegmentController.cs
@@ -19,7 +19,9 @@ public class WaveSegmentController : MonoBehaviour {
     }
 
     void Update() {
-        if (transform.position.x > 200 && transform.position.x < 400) {
+        if ((transform.position.x > 200 && transform.position.x < 400) ||
+			(transform.position.x > 1500 && transform.position.x < 1700) ||
+			(transform.position.x > 3100 && transform.position.x < 3300)) {
             if (refractiveIndex != 0.5f) {
                 refractiveIndex = 0.5f;
                 rend.material.SetFloat("_RefractiveIndex", 0.5f);
@@ -38,16 +40,30 @@ public class WaveSegmentController : MonoBehaviour {
         float predictedX = transform.position.x;
 
         if (velocity != 0) {
-            predictedX = transform.position.x + velocity * refractiveIndex * Time.deltaTime;
+            predictedX += velocity * refractiveIndex * Time.deltaTime;
 		
-            if (predictedX > 200.0f && refractiveIndex != 0.5f) {
+            if (predictedX > 200.0f && predictedX < 400.0f && refractiveIndex != 0.5f) {
                 predictedX += (200.0f - transform.position.x) +
                     (Time.deltaTime - (200.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 0.5f;
-            } else if (predictedX > 400.0f && refractiveIndex != 1.0f) {
-                predictedX += (400.0f - transform.position.x) +
-                    (Time.deltaTime - (400.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
-            }
-        }
+            } else if (predictedX > 1500.0f && predictedX < 1700.0f && refractiveIndex != 0.5f) {
+                predictedX += (1500.0f - transform.position.x) +
+                    (Time.deltaTime - (1500.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 0.5f;
+			} else if (predictedX > 3100.0f && predictedX < 3300.0f && refractiveIndex != 0.5f) {
+                predictedX += (3100.0f - transform.position.x) +
+                    (Time.deltaTime - (3100.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 0.5f;
+			} else if (refractiveIndex != 1.0f) {
+				if (predictedX > 400.0f && predictedX < 1500.0f) {
+					predictedX += (400.0f - transform.position.x) +
+					(Time.deltaTime - (400.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
+				} else if (predictedX > 1700.0f && predictedX < 3100.0f) {
+					predictedX += (1700.0f - transform.position.x) +
+					(Time.deltaTime - (1700.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
+				} else if (predictedX > 3300.0f) {
+					predictedX += (3300.0f - transform.position.x) +
+					(Time.deltaTime - (3300.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
+				}
+			}
+		}
 		
         if (Input.GetAxis ("Vertical") > 0) {
             velocity += deltaV;
