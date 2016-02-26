dea6ed35403f2f9a3df6dd5c5320845d4d09c738
grantaddis <grant@addis.net>
2016-02-26 01:15:09 -0500

    I think velocity modulation is working?

--- Assets/Scripts/WaveSegmentController.cs
+++ Assets/Scripts/WaveSegmentController.cs
@@ -19,13 +19,15 @@ public class WaveSegmentController : MonoBehaviour {
     }
 
     void Update() {
-        if (transform.position.x > 100 && transform.position.x < 200) {
+        if (transform.position.x > 200 && transform.position.x < 400) {
             if (refractiveIndex != 0.5f) {
                 refractiveIndex = 0.5f;
+                rend.material.SetFloat("_RefractiveIndex", 0.5f);
             }
         } else {
             if (refractiveIndex != 1.0f) {
                 refractiveIndex = 1.0f;
+                rend.material.SetFloat("_RefractiveIndex", 1.0f);
             }
         }
 
@@ -38,12 +40,12 @@ public class WaveSegmentController : MonoBehaviour {
         if (velocity != 0) {
             predictedX = transform.position.x + velocity * refractiveIndex * Time.deltaTime;
 		
-            if (predictedX > 100.0f && refractiveIndex != 0.5f) {
-                predictedX += (100.0f - transform.position.x) +
-                    (Time.deltaTime - (100.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 0.5f;
-            } else if (predictedX > 200.0f && refractiveIndex != 1.0f) {
+            if (predictedX > 200.0f && refractiveIndex != 0.5f) {
                 predictedX += (200.0f - transform.position.x) +
-                    (Time.deltaTime - (200.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
+                    (Time.deltaTime - (200.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 0.5f;
+            } else if (predictedX > 400.0f && refractiveIndex != 1.0f) {
+                predictedX += (400.0f - transform.position.x) +
+                    (Time.deltaTime - (400.0f - transform.position.x) / (velocity * refractiveIndex)) * velocity * 1.0f;
             }
         }
 		
